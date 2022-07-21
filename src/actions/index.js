084b4ff4ff11719af6f1db425fe22596577a32d4
Mukul Saini <thisismemukul@gmail.com>
2022-07-21 10:29:21 +0530

    First Action

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+const GET_ALL_PRODUCT_REQUEST = 'GET_ALL_PRODUCT_REQUEST';
+
+function getAllProductRequest() {
+    return {
+        type: GET_ALL_PRODUCT_REQUEST,
+        info: 'first redux action'
+
+    }
+}
