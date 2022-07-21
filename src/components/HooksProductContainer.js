5fbf73444348c326dc84e3d4b263de42afaaca29
Mukul Saini <thisismemukul@gmail.com>
2022-07-21 13:39:08 +0530

    useDispatch

--- src/components/HooksProductContainer.js
+++ src/components/HooksProductContainer.js
@@ -1,11 +1,14 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { GetProductAction } from  '../redux'
+
 const HooksProductContainer = () => {
     const numOfProducts = useSelector(state => state.numOfProducts)
+    const dispatch = useDispatch();
     return (
         <>
             <h3>No of Products- {numOfProducts}</h3>
-            <button>Buy product </button>
+            <button onClick={()=>dispatch(GetProductAction())}>Buy product using hooks </button>
         </>
     )
 }
