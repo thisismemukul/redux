0108dedc5d94059a5908906dbb946370d344e85d
Mukul Saini <thisismemukul@gmail.com>
2022-07-21 10:36:43 +0530

    first reducer

--- src/actions/index.js
+++ src/actions/index.js
@@ -7,3 +7,25 @@ function getAllProductRequest() {
 
     }
 }
+
+//(previousState, action) => newState
+
+const initialState = {
+    products: 10,
+    loading: false,
+    error: false,
+    message: '',
+}
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case GET_ALL_PRODUCT_REQUEST:
+            return {
+                ...state,
+                products: state.products + 1,
+                loading: true,
+            }
+        default:
+            return state;
+    }
+}
