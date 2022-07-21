d03bfa618e1a1e23d308c80e05fcd22f4151c81e
Mukul Saini <thisismemukul@gmail.com>
2022-07-21 12:48:57 +0530

    Creating store and initializing Provider

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import './App.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import ProductContainer from './components/ProductContainer';
 
 const App = () => {
   return (
-    <div className='App'>
-      <ProductContainer />
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <ProductContainer />
+      </div>
+    </Provider>
   )
 }
 
